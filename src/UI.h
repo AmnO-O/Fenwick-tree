140b6ebc5267cbdd05d718b85f9b0b1e8954e17a
Phạm Hữu Nam <nam722006@gmail.com>
2025-05-02 18:09:52 +0700

    Update UI.h

--- src/UI.h
+++ src/UI.h
@@ -1,7 +1,28 @@
+#ifndef UI_H
+#define UI_H
+
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
 
-using namespace std;
+void printHelp();
+void clearScreen();
 
-void printHelp(); // print help message
-void process(); 
+int getIntInput(const std::string& prompt, int minValue = std::numeric_limits<int>::min(),
+    int maxValue = std::numeric_limits<int>::max());
 
+std::string getStringInput(const std::string& prompt);
+
+bool getYesNoInput(const std::string& prompt);
+
+void handleGenerateTestCase();
+void handleGenerateSmallTest();
+void handleRunTestCase();
+void handleVerifyTestCase();
+void handleBenchmarkTestCase();
+void handlePrintTestCase();
+void handleInputTestCase();
+void process();
+
+#endif // UI_H
